760d0e32023c6bd889949a877c7941ae97a342b2
Prakhar Vaibhav <prakharvaibhav31@gmail.com>
2024-04-08 18:17:27 +0530

    Changes for adding new StateCityReposne class

---
+++ src/main/java/com/bus/BusService/response/StateCityResponse.java
@@ -0,0 +1,15 @@
+package com.bus.BusService.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StateCityResponse {
+
+	private long id;
+
+	private String name;
+}
